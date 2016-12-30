d599501ac5091fca25c9525a2d2d2533b3727d8f
Paolo Bianchi <paolo.bianchi@it.gucci.com>
2016-12-30 11:06:52 +0100

    Added manifest command

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ import sys
 from setuptools import setup
 
 PACKAGES = ['pyipa']
-SCRIPTS = ['bin/pyipa']
+
 
 def get_init_val(val, packages=PACKAGES):
     pkg_init = "%s/__init__.py" % PACKAGES[0]
@@ -36,17 +36,22 @@ def get_init_val(val, packages=PACKAGES):
         if line.startswith(value):
             return line.split('=')[1].strip().strip("'")
 
+
 setup(
-    name                = get_init_val('title'),
-    version             = get_init_val('version'),
-    description         = get_init_val('description'),
-    long_description    = open('README.rst').read(),
-    author              = get_init_val('author'),
-    author_email        = get_init_val('author_email'),
-    url                 = get_init_val('url'),
-    package_data        = {'': ['LICENSE', 'NOTICE']},
-    license             = get_init_val('license'),
-    install_requires    = ['docopt'],
-    packages            = PACKAGES,
-    scripts             = SCRIPTS
+    name=get_init_val('title'),
+    version=get_init_val('version'),
+    description=get_init_val('description'),
+    long_description=open('README.rst').read(),
+    author=get_init_val('author'),
+    author_email=get_init_val('author_email'),
+    url=get_init_val('url'),
+    package_data={'': ['LICENSE', 'NOTICE']},
+    license=get_init_val('license'),
+    install_requires=['docopt'],
+    packages=PACKAGES,
+    entry_points={
+        'console_scripts': [
+            'pyipa = pyipa.main:main'
+        ]
+    }
 )
